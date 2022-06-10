2d951286510a0c5487436282dfe94593fd241ea3
Willyanderson <willyanderson.medeiros@gmail.com>
2022-06-10 17:03:13 -0300

    criei o primeiro desafio da aula 12

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 6 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#a0675c'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#151829'
+    }
+}
