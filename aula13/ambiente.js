58d8b74578ba38fe780159eedbb31b5bf5df618b
Willyanderson <willyanderson.medeiros@gmail.com>
2022-06-13 14:42:52 -0300

    criei os conteúdos relacionados as aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+
+// Estrutura de Repetição com Teste Lógico no INÍCIO
+ while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c += 1
+}
+
+// Estrutura de Repetição com Teste Lógico no FINAL
+/* do {
+    console.log(`Passo ${c}`)
+    c += 1
+} while (c <= 6)
+*/
